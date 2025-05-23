11b3c8f693aa21e55281d1a908876489cf723c3b
mizsada <190970894+mizsada@users.noreply.github.com>
2025-05-24 03:39:31 +0800

    chore: update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default defineConfig([
   js.configs.recommended,
-  globalIgnores(['eslint-ignore/**']),
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ClipboardJS: 'readonly',
+        GLightbox: 'readonly',
+        Theme: 'readonly',
+        dayjs: 'readonly',
+        mermaid: 'readonly',
+        tocbot: 'readonly',
+        importScripts: 'readonly',
+        swconf: 'readonly'
+      }
+    },
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single']
